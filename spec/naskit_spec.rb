b5cd7604ce41283dc5e0a3bd4facc311266dda50
Guillaume Malette <guillaume.malette@jadedpixel.com>
2013-05-29 21:06:15 -0400

    Nest tests in describe block

--- spec/naskit_spec.rb
+++ spec/naskit_spec.rb
@@ -27,41 +27,47 @@ describe Naskit::API::WWW do
 end
 
 describe Naskit::App do
-  source = File.expand_path("../assets", __FILE__)
-  destination = File.expand_path("../result-assets", __FILE__)
+  describe "source and destination" do
+    source = File.expand_path("../assets", __FILE__)
+    destination = File.expand_path("../result-assets", __FILE__)
 
-  subject {
-    Naskit::App.new :source       => source,
-                    :destination  => destination,
-                    :convert      => true,
-                    :extensions   => Naskit::DEFAULT_EXTENSIONS + ["mpeg"],
-                    :format       => Naskit::DEFAULT_FORMAT
-  }
+    subject {
+      Naskit::App.new :source       => source,
+                      :destination  => destination,
+                      :convert      => true,
+                      :extensions   => Naskit::DEFAULT_EXTENSIONS + ["mpeg"],
+                      :format       => Naskit::DEFAULT_FORMAT
+    }
 
-  its(:files) { should have(2).members }
+    its(:files) { should have(2).members }
 
 
-  context "#run" do
-    before { subject.run }
-    after { FileUtils.rm_r(destination) }
+    context "#run" do
+      before { subject.run }
+      after { FileUtils.rm_r(destination) }
 
-    it "creates a destination tree" do
-      File.exist?(destination).should be_true
-      File.directory?(destination).should be_true
+      it "creates a destination tree" do
+        File.exist?(destination).should be_true
+        File.directory?(destination).should be_true
+      end
+
+      it "copies the video it doesn't need to transcode" do
+        source_file = File.join(source, "empty.m4v")
+        destination_file = File.join(destination, "Empty videos/1/1. Empty.m4v")
+        Digest::MD5.file(source_file).hexdigest.should == Digest::MD5.file(destination_file).hexdigest
+      end
+
+      it "transcode the mpeg" do
+        source_file = File.join(source, "How.I.Met.Your.Mother.S01E01.mpeg")
+        destination_file = File.join(destination, "How I Met Your Mother/1/1. Pilot.m4v")
+        File.exist?(destination_file).should be_true
+        Digest::MD5.file(source_file).hexdigest.should_not == Digest::MD5.file(destination_file).hexdigest
+      end
     end
+  end
 
-    it "copies the video it doesn't need to transcode" do
-      source_file = File.join(source, "empty.m4v")
-      destination_file = File.join(destination, "Empty videos/1/1. Empty.m4v")
-      Digest::MD5.file(source_file).hexdigest.should == Digest::MD5.file(destination_file).hexdigest
-    end
+  describe "--find-missing" do
 
-    it "transcode the mpeg" do
-      source_file = File.join(source, "How.I.Met.Your.Mother.S01E01.mpeg")
-      destination_file = File.join(destination, "How I Met Your Mother/1/1. Pilot.m4v")
-      File.exist?(destination_file).should be_true
-      Digest::MD5.file(source_file).hexdigest.should_not == Digest::MD5.file(destination_file).hexdigest
-    end
   end
 end
 
